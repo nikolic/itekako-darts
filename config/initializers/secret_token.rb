9961524793d3d6187f819f2ecfe65da2640341ae
nikolic <nikolic89@hotmail.com>
2012-12-26 12:29:54 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItekakoDarts::Application.config.secret_token = '0aefab79f15c2df7f2fb8b56b6448c094a00f2c4cc1829fd0f300faf512cb9c5c286f6ab083261936f19c9b1a3e898609c9501a20cdb01ed4ab9b1342dcfa5a0'
