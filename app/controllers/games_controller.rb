83c774229f1cccf1dcf242ab9a0b4b159de9471c
moysa <b_mojsilovic@yahoo.com>
2012-12-28 14:11:23 +0100

    resolved conflicts

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,16 +4,6 @@ class GamesController < ApplicationController
  
     player_ids = params[:players]
 
-<<<<<<< HEAD
-    if not player_ids.blank? 
-      game = Game.create()
-      Participation.create_participations(game.id, player_ids)
-      redirect_to :controller => 'games', :action => 'index'
-    else
-      redirect_to :controller => 'games', :action => 'options'
-    end 
-      
-=======
   	if not player_ids.blank? 
   		game = Game.create()
   		Participation.create_participations(game.id, player_ids)
@@ -21,18 +11,11 @@ class GamesController < ApplicationController
   	else
   		redirect_to :controller => 'games', :action => 'options'
   	end 
-  		
->>>>>>> 6de11aabc57769890bb5e6748b733220500c3c61
   end
 
 
   def options
-<<<<<<< HEAD
     @players = Player.all
-    
-=======
-    @players = Player.get_all_players   
->>>>>>> 6de11aabc57769890bb5e6748b733220500c3c61
   end
 
 
