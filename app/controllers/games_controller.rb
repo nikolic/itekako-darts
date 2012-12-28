6de11aabc57769890bb5e6748b733220500c3c61
Marko Nikolic <nikolic89@hotmail.com>
2012-12-28 13:48:47 +0100

    Routes problem fixed.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,25 +7,27 @@ class GamesController < ApplicationController
   	if not player_ids.blank? 
   		game = Game.create()
   		Participation.create_participations(game.id, player_ids)
-  		redirect_to :controller => 'games', :action => 'index'
+  		redirect_to :controller => 'games', :action => 'index', :id => game.id
   	else
   		redirect_to :controller => 'games', :action => 'options'
   	end 
   		
   end
 
+
+  def options
+    @players = Player.get_all_players   
+  end
+
+
+  def index
+    game_id = params[:id]
+    @game = Game.find(game_id)
+  end
+
+
   def show
     
   end
-
-  def options
-    @players = Player.get_all_players
-    
-  end
-
-  def index
-    
-  end
-
 
 end
